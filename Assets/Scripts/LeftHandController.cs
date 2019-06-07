b0596643d51b890e31341dcb8b15843ba7184db5
Janislav <jan.boerner@nexiles.com>
2019-06-07 11:49:33 +0200

    changed to freq

--- Assets/Scripts/LeftHandController.cs
+++ Assets/Scripts/LeftHandController.cs
@@ -8,18 +8,22 @@ public class LeftHandController : MonoBehaviour {
 
     private Controller controller;
     public AudioHelm.HelmController helmController;
+    public int bpm;
 
-    private float time1;
-    private float time2;
+    private float lastFrequency;
 
 	// Use this for initialization
 	void Start () {
         controller = new Controller();
+        float invertedValue = bpm / 60;
+        float intervall = 1 / invertedValue;
+        Debug.Log("Intervall :" + intervall);
+        InvokeRepeating("Bang", 0.0f, intervall);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (controller.IsConnected) {
+
+    void Bang() {
+        if (controller.IsConnected)
+        {
             Frame frame = controller.Frame();
             HandList hands = frame.Hands;
             //PointableList pointables = frame.Pointables;
@@ -27,11 +31,17 @@ public class LeftHandController : MonoBehaviour {
             //ToolList tools = frame.Tools;
             Hand leftHand = hands[0];
             if (leftHand.IsLeft) {
+                helmController.FrequencyOff(lastFrequency);
                 Vector position = leftHand.PalmPosition;
-                int note = (int) position.y;
-                helmController.NoteOn(note, 1.0f, 0.1f);
-                Debug.Log(position);
+                int note = (int)position.y;
+                helmController.FrequencyOn(position.y);
+                //helmController.NoteOn(note, 1.0f, 0.1f);
+                //helmController.NoteOn(note + 3, 1.0f, 0.1f);
+                lastFrequency = position.y;
+                //Debug.Log(position);
+            } else {
+                helmController.FrequencyOff(lastFrequency);
             }
         }
-	}
+    }
 }

fcda4c61b1024aa1812d63246b9a4fd0fc8c7542
Abdulmasih <abdalmasih.alkhasi@gmail.com>
2019-06-28 10:37:22 +0200

    Add the Mat , Scr , Mesh and Prefabs of visualisation

---
+++ Assets/ParamCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+    public int _band;
+    public float _startscale, _scaleMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, (AudioScript._freqBand[_band]* _scaleMultiplier)+_startscale,transform.localScale.z);
+    }
+}
---
+++ Assets/ScrollingTexture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingTexture : MonoBehaviour
+{
+    float moveX;
+    float moveZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        moveX = 0.5f * Time.deltaTime + moveX;
+        moveZ = 0.5f * Time.deltaTime + moveZ;
+        if (moveX > 1) moveX = 0;
+        if (moveZ > 1) moveZ = 0;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector3(moveZ, moveX, moveZ);
+    }
+}
